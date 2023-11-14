db95a6513d94bf567311c5f03863961accd9ee4f
tfilewich <105830689+tfilewich@users.noreply.github.com>
2023-11-14 14:47:27 -0700

    Lay out Factory pattern

---
+++ src/main/java/Farm.java
@@ -0,0 +1,22 @@
+/**
+ * Common interface for all farms.
+ * The "Product" of the Factory pattern
+ * The "Component" of the Decorator pattern.
+ * @author tfilewic
+ */
+public interface Farm {
+    
+    /**
+     * Updates the state of the farm by one 12hr tick.
+     */
+    void tick(boolean isDay);
+    
+    /**
+     * Earns the farm passive income from its operations;
+     */
+    void earn();
+   
+   
+    //TODO : concrete implementations
+}
+ 
---
+++ src/main/java/FarmFactory.java
@@ -0,0 +1,16 @@
+/**
+ * Abstract class to create Farm objects.
+ * The "Creator" class in the Factory pattern
+ * @author tfilewic
+ */
+public abstract class FarmFactory {
+    
+    /**
+     * Creates a new Farm.
+     * The factoryMethod() from the Factory pattern.
+     * @return a new Farm.
+     */
+    public abstract Farm createFarm();
+    
+    //TODO : concrete implementations
+}
